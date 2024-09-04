b2b96433608acada6a9615ef50ecb3d503cb15fc
flexOfficial20 <155304501+flexOfficial20@users.noreply.github.com>
2024-09-04 22:46:13 +0530

    Update __init__.py

--- shivu/__init__.py
+++ shivu/__init__.py
@@ -30,8 +30,8 @@ UPDATE_CHAT = Config.UPDATE_CHAT
 BOT_USERNAME = Config.BOT_USERNAME 
 sudo_users = Config.sudo_users
 OWNER_ID = Config.OWNER_ID 
-JOINLOGS = "-1002041586214"
-LEAVELOGS = "-1001977783984"
+JOINLOGS = "-1002078575375"
+LEAVELOGS = "-1002078575375"
 
 application = Application.builder().token(TOKEN).build()
 shivuu = Client("Shivu", api_id, api_hash, bot_token=TOKEN)
