3cbaa5c5047b8cd01f4f6f2c4e6a9cc4028cc70a
flexOfficial20 <155304501+flexOfficial20@users.noreply.github.com>
2024-09-04 22:43:26 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,18 +2,18 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "7011990425"
-    sudo_users = ["6890857225", "7011990425", "6264760837", "5527022252", "6384484443", "1850686769"]
-    GROUP_ID = "-1002041586214"
-    TOKEN = "your bot token"
-    mongo_url = "your db"
-    PHOTO_URL = ["https://telegra.ph/file/7e5398823512d307128a3.jpg", "https://telegra.ph/file/c45dcb207d81e97cb4f6a.jpg", "https://telegra.ph/file/0bc6d65878e8300fbf0f8.jpg", "https://telegra.ph/file/0afb45203ff162ee7227b.jpg"]
-    SUPPORT_CHAT = "lustsupport"
-    UPDATE_CHAT = "lustxUpdate"
-    BOT_USERNAME = "lustXcatcherrobot"
-    CHARA_CHANNEL_ID = "-1002023474262"
-    api_id = "20457610"
-    api_hash = "b7de0dfecd19375d3f84dbedaeb92537"
+    OWNER_ID = "6584789596"
+    sudo_users = ["5630057244", "2010819209", "5702598840", "6101457748", "6154972031", "1735664760", "7036005233", "6100011620", "7297953309", "6412447141", "7244871367", "5530116994", "6584789596", "949302414"]
+    GROUP_ID = "-1002000314620"
+    TOKEN = "6600186454:AAE066bOjW327LQX5WsIACvTz8NCK5yBal4"
+    mongo_url = "mongodb+srv://Epic2:w85NP8dEHmQxA5s7@cluster0.tttvsf9.mongodb.net/?retryWrites=true&w=majority"
+    PHOTO_URL = ["https://telegra.ph//file/e64337bbc6cdac7e6b178.jpg", "https://telegra.ph/file/ed23556d07d33db18402d.jpg", "https://telegra.ph//file/32556c77847dff110577c.jpg", "https://telegra.ph//file/0650844fc5db4049959bc.jpg"]
+    SUPPORT_CHAT = "Grabbing_Your_WH_Group"
+    UPDATE_CHAT = "FLEX_BOTS_NEWS"
+    BOT_USERNAME = Grabbing_Your_Waifu_Bot"
+    CHARA_CHANNEL_ID = "-1002009998662"
+    api_id = "20533795"
+    api_hash = "f6cadf28523943f525e706e6ace8a250"
 
     
 class Production(Config):
