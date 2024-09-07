576c9458026e93b7ddc62a713fab95ce56140700
flexOfficial20 <155304501+flexOfficial20@users.noreply.github.com>
2024-09-07 21:13:11 +0530

    Update transfer.py

--- shivu/modules/transfer.py
+++ shivu/modules/transfer.py
@@ -86,4 +86,4 @@ async def transfer_confirm(update, context):
 
 # Register the handlers 
 application.add_handler(CommandHandler("transfer", transfer))
-application.add_handler(CallbackQueryHandler(handle_transfer_confirmation, pattern='^confirm_transfer|^cancel_transfer$'))
+application.add_handler(CallbackQueryHandler(transfer_confirm, pattern='^confirm_transfer|^cancel_transfer$')) # Register with correct function name
