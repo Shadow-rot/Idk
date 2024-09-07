cba6178ae760c34ae688e2a1358e4c8176f4fc60
flexOfficial20 <155304501+flexOfficial20@users.noreply.github.com>
2024-09-07 21:32:11 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -279,7 +279,7 @@ def main() -> None:
     application.add_handler(CommandHandler('set_off', set_off, block=False))
     application.add_handler(MessageHandler(filters.ALL, message_counter, block=False))
     PORT = int(os.environ.get("PORT", "8443"))
-    TOKEN = os.environ.get("TOKEN")
+    TOKEN = os.environ.get("render.com")
     RENDER_EXTERNAL_URL = os.environ.get("RENDER_EXTERNAL_URL")
     if not TOKEN or not RENDER_EXTERNAL_URL:
         LOGGER.error("Environment variables TOKEN or RENDER_EXTERNAL_URL are not set")
