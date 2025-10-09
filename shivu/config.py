5dbacb287eae90e9d95c7ab19d804f80058df111
Shadow-rot <looktouhid@gmail.com>
2025-10-09 22:33:44 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,18 +2,18 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "6584789596"
-    sudo_users = ["5630057244", "2010819209", "5702598840", "6101457748", "6154972031", "1735664760", "7036005233", "6100011620", "7297953309", "6412447141", "7244871367", "5530116994", "6584789596", "949302414"]
-    GROUP_ID = "-1002000314620"
-    TOKEN = "6600186454:AAFLZH3zYUM3V1q_a7RkQ6nnGlHiz5hRxoU"
+    OWNER_ID = "5147822244"
+    sudo_users = ["6507226414", "7938543259", "5147822244",]
+    GROUP_ID = "-1002191083108"
+    TOKEN = "7891572866:AAGNYgwJ8cS0UJzjsWbQkszQPKiUhSwLsfw"
     mongo_url = "mongodb+srv://Epic2:w85NP8dEHmQxA5s7@cluster0.tttvsf9.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph//file/e64337bbc6cdac7e6b178.jpg", "https://telegra.ph/file/ed23556d07d33db18402d.jpg", "https://telegra.ph//file/32556c77847dff110577c.jpg", "https://telegra.ph//file/0650844fc5db4049959bc.jpg"]
-    SUPPORT_CHAT = "Grabbing_Your_WH_Group"
-    UPDATE_CHAT = "FLEX_BOTS_NEWS"
-    BOT_USERNAME = "Grabbing_Your_Waifu_Bot"
-    CHARA_CHANNEL_ID = "-1002009998662"
-    api_id = "20533795"
-    api_hash = "f6cadf28523943f525e706e6ace8a250"
+    SUPPORT_CHAT = "siya_infoo"
+    UPDATE_CHAT = "siya_infoo"
+    BOT_USERNAME = "waifukunbot"
+    CHARA_CHANNEL_ID = "-1002059929123"
+    api_id = "17944283"
+    api_hash = "03f2f561ca86def71fe88d3ae16ed529"
 
     
 class Production(Config):
