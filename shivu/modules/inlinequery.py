0871422866a6b4252c0d2cc4010513fae61fb692
Shadow-rot <looktouhid@gmail.com>
2025-10-10 00:34:01 +0530

    Update inlinequery.py

--- shivu/modules/inlinequery.py
+++ shivu/modules/inlinequery.py
@@ -114,12 +114,12 @@ if query.startswith('collection.') and user_id.isdigit() and user:
     user_character_count = sum(c['id'] == character['id'] for c in user['characters'])
     user_anime_count = sum(c['anime'] == character['anime'] for c in user['characters'])
 
-            caption = (
-                f"<b>Look at <a href='tg://user?id={user['id']}'>{escape(user.get('first_name', user['id']))}</a>'s Waifu!</b>\n\n"
-                f"<b>{character['id']}:</b> {character['name']} x{user_character_count}\n"
-                f"<b>{character['anime']}</b> {user_anime_count}/{anime_characters}\n"
-                f"﹙<b>{character['rarity'][0]} RARITY:</b> {character['rarity'][2:]})\n\n"
-            )
+    caption = (
+        f"<b>Look at <a href='tg://user?id={user['id']}'>{escape(user.get('first_name', user['id']))}</a>'s Waifu!</b>\n\n"
+        f"<b>{character['id']}:</b> {character['name']} x{user_character_count}\n"
+        f"<b>{character['anime']}</b> {user_anime_count}/{anime_characters}\n"
+        f"﹙<b>{character['rarity'][0]} RARITY:</b> {character['rarity'][2:]})\n\n"
+    )
         else:
             caption = (
                 f"<b>Look at this Waifu!</b>\n\n"
