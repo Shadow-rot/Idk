51465c2626c192334a9b59e68499b11090b2975f
Shadow-rot <looktouhid@gmail.com>
2025-10-11 21:17:35 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,18 +2,18 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "5147822244"
-    sudo_users = ["6507226414", "7938543259", "5147822244",]
+    OWNER_ID = "8297659126"
+    sudo_users = ["8297659126", "8420981179", "5147822244",]
     GROUP_ID = "-1002191083108"
     TOKEN = "7891572866:AAEKgMqTNK0vQ_mAw63YFKdL6bD2oEiss14"
     mongo_url = "mongodb+srv://Epic2:w85NP8dEHmQxA5s7@cluster0.tttvsf9.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph//file/e64337bbc6cdac7e6b178.jpg", "https://telegra.ph/file/ed23556d07d33db18402d.jpg", "https://telegra.ph//file/32556c77847dff110577c.jpg", "https://telegra.ph//file/0650844fc5db4049959bc.jpg"]
-    SUPPORT_CHAT = "siya_infoo"
-    UPDATE_CHAT = "siya_infoo"
+    SUPPORT_CHAT = "PICK_X_SUPPORT"
+    UPDATE_CHAT = "PICK_X_UPDATE"
     BOT_USERNAME = "waifukunbot"
     CHARA_CHANNEL_ID = "-1002059929123"
-    api_id = "17944283"
-    api_hash = "03f2f561ca86def71fe88d3ae16ed529"
+    api_id = "21705508"
+    api_hash = "1d590f4c3d2029a7ef7df087707d7441"
 
     
 class Production(Config):
