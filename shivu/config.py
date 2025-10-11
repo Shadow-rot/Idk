80bf9cc924c093a9bc4a57cd8fb4826428928ca6
Shadow-rot <looktouhid@gmail.com>
2025-10-11 19:48:17 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -5,7 +5,7 @@ class Config(object):
     OWNER_ID = "5147822244"
     sudo_users = ["6507226414", "7938543259", "5147822244",]
     GROUP_ID = "-1002191083108"
-    TOKEN = "7891572866:AAGNYgwJ8cS0UJzjsWbQkszQPKiUhSwLsfw"
+    TOKEN = "7535325516:AAEyxQHqlrKMAChi4svzzxtQgtAwjOZMjfI"
     mongo_url = "mongodb+srv://Epic2:w85NP8dEHmQxA5s7@cluster0.tttvsf9.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph//file/e64337bbc6cdac7e6b178.jpg", "https://telegra.ph/file/ed23556d07d33db18402d.jpg", "https://telegra.ph//file/32556c77847dff110577c.jpg", "https://telegra.ph//file/0650844fc5db4049959bc.jpg"]
     SUPPORT_CHAT = "siya_infoo"
