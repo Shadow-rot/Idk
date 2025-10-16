cbeaf88f5cc0052effbed4b5987c9a9f5aeeff74
Shadwoo <looktouhid@gmail.com>
2025-10-16 19:02:03 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -24,7 +24,7 @@ from shivu.modules.kill import register_kill_handler
 from shivu.modules.hclaim import register_hclaim_handler
 from shivu.modules.gift import register_gift_handlers
 from shivu.modules.fav import register_favorite_handlers
-from shivu.modules.ps import ps.register_handlers(application)
+from shivu.modules import ps
 
 # Database collections
 collection = db['anime_characters_lol']
