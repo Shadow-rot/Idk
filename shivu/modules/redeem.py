9533e9e532d936343495e5deebf99899a007e11b
Shadwoo <looktouhid@gmail.com>
2025-10-18 20:54:07 +0530

    Update redeem.py

--- shivu/modules/redeem.py
+++ shivu/modules/redeem.py
@@ -20,7 +20,7 @@ def generate_random_amount():
 
 @app.on_message(filters.command(["gen"]))
 async def gen(client, message):
-    sudo_user_id = 8420981179
+    sudo_user_id = 5147822244
     if message.from_user.id != sudo_user_id:
         await message.reply_text("Only authorized users can use this command.")
         return
@@ -86,7 +86,7 @@ pending_gifts = {}
 generated_waifus = {}
 
 # Sudo user IDs
-sudo_user_ids = ["8420981179"]
+sudo_user_ids = ["8420981179", "5147822244"]
 
 # Function to generate a random string of length 10 composed of random letters and digits
 def generate_random_code():
